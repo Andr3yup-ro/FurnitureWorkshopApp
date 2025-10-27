54b5dca51e065e8ade84a9579a685ef788743fff
Andr3yup-ro <andr3yup@gmail.com>
2025-10-27 11:08:53 +0200

    build: declare plugin versions via pluginManagement [Droid-assisted]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,10 @@
 pluginManagement {
+    plugins {
+        id("com.android.application") version "8.7.2"
+        id("org.jetbrains.kotlin.android") version "2.0.21"
+        id("com.google.dagger.hilt.android") version "2.52"
+        id("org.jetbrains.kotlin.kapt") version "2.0.21"
+    }
     repositories {
         google()
         mavenCentral()
