a6c8fcfde87c86668b07889aeb4c208348f40244
Andr3yup-ro <andr3yup@gmail.com>
2025-10-27 11:10:44 +0200

    build: enable Kotlin Compose compiler plugin (Kotlin 2.0) [Droid-assisted]

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,6 +2,7 @@ pluginManagement {
     plugins {
         id("com.android.application") version "8.7.2"
         id("org.jetbrains.kotlin.android") version "2.0.21"
+        id("org.jetbrains.kotlin.plugin.compose") version "2.0.21"
         id("com.google.dagger.hilt.android") version "2.52"
         id("org.jetbrains.kotlin.kapt") version "2.0.21"
     }
